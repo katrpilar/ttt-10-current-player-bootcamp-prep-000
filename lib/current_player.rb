b3f9df829c5a1a646d367ab44fad795376fbd201
Katrina Brinson <kbrinso@gmail.com>
2018-01-12 06:10:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  counter = 1
+  counter = 0
   board.each{|loc| 
     if (loc == "X") || (loc == "O")
       counter +=1
@@ -10,8 +10,8 @@ end
 
 def current_player(board)
   if turn_count(board).odd?
-    return "X"
-  else
     return "O"
+  else
+    return "X"
   end
 end
